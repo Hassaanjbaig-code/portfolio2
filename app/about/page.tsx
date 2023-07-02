6bcf84e6dc2d8d47d3f9bf4a7a212ebd7f43d6c2
Hassaanjbaig-code <hassaanb000@gmail.com>
2023-07-02 11:33:07 -0700

    Working of the page

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image"
+
+const page = () => {
+  return (
+    <div className='bg-gray-500 h-[calc(100vh-4rem)] overflow-hidden'>
+        <h1 className='font-Belanosima font-bold text-5xl text-center'>About Myself</h1>
+    </div>
+  )
+}
+
+export default page
