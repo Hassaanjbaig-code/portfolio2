14eefaddfe214a7462fe8ebb421eb96ca99f04d8
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-07-02 17:06:57 +0500

    Working of the project page

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,9 +1,51 @@
-import Image from "next/image"
+'use client'
+'use '
+
+import Image from "next/image";
+import { projectdata } from "@/constants";
+import Link from "next/link";
 
 const Card = () => {
+  const slice = (text: string, maxWords: number) => {
+    const words = text.split(" ");
+    const slicedWords = words.slice(0, maxWords);
+    return slicedWords.join(" ");
+  };
   return (
-    <div>
-      Hello World
+    <div className="flex justify-center items-center w-full flex-col my-22">
+      {projectdata.map((data) => (
+        <div className="flex w-[80%] justify-between items-center gap-x-5 p-6" key={data.id}>
+          <div className="flex w-[91rem] h-[14rem]">
+            <Image src={data.Image} alt="Logo" width={300} height={300} className="object-contain self-center" />
+          </div>
+          <div>
+            <div className="">
+              <h2>{data.title}</h2>
+              <p>{ slice(data.description, 50) }</p>
+            </div>
+            <div>
+              <button type="button">
+                <Link
+                  key={data.title}
+                  href={`${data.Live_demo}`}
+                  type="Live_demo"
+                  className="text-gray-500"
+                >
+                  Live Demo
+                </Link>
+              </button>
+              <Link
+                key={data.title}
+                href={`${data.Urls}`}
+                type="Url"
+                className="text-gray-500"
+              >
+                See the source code
+              </Link>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
