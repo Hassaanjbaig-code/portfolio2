7d268f615f3d00feb4734996083a03953de13110
Hassaanjbaig-code <hassaanb000@gmail.com>
2023-07-08 05:13:02 -0700

    Fix a color issue

--- components/Form.tsx
+++ components/Form.tsx
@@ -14,26 +14,26 @@ const Form = () => {
             <form onSubmit={handleSubmit} className="text-white flex flex-col gap-4">
                 <legend className="font-serif font-semibold">Message me</legend>
                 <span className="gap-4 flex max-md:flex-col">
-                    <input type="text" id='name' name='name' placeholder="Name" required className="bg-black w-[14rem] h-[2rem] border-b border-border p-2 placeholder-primary" />
+                    <input type="text" id='name' name='name' placeholder="Name" required className="bg-[#80808069] w-[14rem] h-[2rem] border-b border-border p-2 placeholder-primary" />
                     <ValidationError
                         prefix="Name"
                         field="name"
                         errors={state.errors}
                     />
-                    <input type="email" id='email' name='email' placeholder="Email" required className="bg-black w-[14rem] h-[2rem] border-b border-border p-2 placeholder-primary" />
+                    <input type="email" id='email' name='email' placeholder="Email" required className="bg-[#80808069] w-[14rem] h-[2rem] border-b border-border p-2 placeholder-primary" />
                     <ValidationError
                         prefix="Email"
                         field="email"
                         errors={state.errors}
                     />
                 </span>
-                <input type="text" id='subject' name='subject' placeholder="Subject" required className="bg-black w-[29rem] h-[2rem] max-md:w-[90%] border-b border-border p-4 placeholder-primary" />
+                <input type="text" id='subject' name='subject' placeholder="Subject" required className="bg-[#808080b4] w-[29rem] h-[2rem] max-md:w-[90%] border-b border-border p-4 placeholder-primary" />
                 <ValidationError
                     prefix="subject"
                     field="subject"
                     errors={state.errors}
                 />
-                <textarea placeholder="Message" id='message' name='message' required className="bg-black w-[29rem] h-28 p-2 placeholder-primary max-md:w-[90%] border-b-[1px] border-border" />
+                <textarea placeholder="Message" id='message' name='message' required className="bg-[#80808069] w-[29rem] h-28 p-2 placeholder-primary max-md:w-[90%] border-b-[1px] border-border" />
                 <ValidationError
                     prefix="Message"
                     field="message"
