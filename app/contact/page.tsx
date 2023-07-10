5f191ab3f1c79cc1e725960be3de205448185c31
Hassaanjbaig-code <hassaanb000@gmail.com>
2023-07-09 22:21:13 -0700

    Fix the issue of height

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,7 +2,7 @@ import { Contact, Form, Personal_Contact } from "@/components"
 
 const page = () => {
     return (
-        <section className="w-full h-[calc(100vh-4rem)] max-md:h-screen flex flex-col justify-around">
+        <section className="w-full h-[calc(100vh-4rem)] max-md:h-full flex flex-col justify-around">
             <h2 className="font-extrabold text-center shadow-sm text-8xl bg-clip-text text-[#fff] mt-10">Contact</h2>
             <div className="w-full md:h-[70%] flex justify-center items-center max-md:items-start max-md:flex-col">
                 <Form />
